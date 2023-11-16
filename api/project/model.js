9c06d465d08fa3610577d5fec08cb6dccaea2c60
DJ1 <jimmynoble604@gmail.com>
2023-11-16 14:31:08 -0500

    completed

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,40 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig')
+
+async function get() {
+    const rows = await db('projects');
+    const result = rows.map(rows => {
+        return {
+            ...row,
+            project_completed: row.project_completed ? true : false 
+        }
+    })
+    return (result)
+}
+
+async function create(project) {
+    const [newID] = await db('projects').insert(project)
+    const newPost = await db('projects').where('project_id', newID)
+    const result = newID.map(row => {
+        return {
+            ...row,
+            project_completed: row.project_completed ? true : false
+        }
+    })
+    return result[0]
+}
+
+async function getById(id) {
+    try {
+        const row = await db('projects')
+        .where('project_id', id)
+        return (row)
+    } catch (error) {
+        return 
+    }
+}
+
+module.exports = {
+    get,
+    create,
+    getById
+}
