8b178a3627db314ae49cdf3cd855ac2d8edc9474
DJ1 <jimmynoble604@gmail.com>
2023-11-16 18:47:23 -0500

    good

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,19 @@
-// build your `/api/resources` router here
+const resource = require('./model')
+
+async function checkResource(req, res, next) {
+    const { resource_name } = req.body
+    const resource = await Resource.getByName(resource_name)
+    try {
+        if(resource.length === 0) {
+            next();
+        } else {
+            next({ status: 400, message: 'Resource name must be unique' })
+        }
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    checkResource
+}
