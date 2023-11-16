eeeddfa06e0831bba4d953b4c7b9fcb65e181316
DJ1 <jimmynoble604@gmail.com>
2023-11-16 12:13:17 -0500

    server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,26 @@
-// build your server here and require it from index.js
+const express = require('express')
+const resourcesRouter = require('./resource/router')
+const projectsRouter = require('./project/router')
+const taskRouter = require('./task/router')
+const server = express();
+
+server.use(express.json())
+
+server.use('./api/resources', resourcesRouter)
+
+server.use('/api/projects', projectsRouter)
+
+server.use('/api/task', taskRouter)
+
+server.get('./', (req, res) => {
+    res.status(200).json({ message: 'connected to server!' })
+})
+// selint-disable-next-line no-unused-bars
+ 
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
+module.exports = server
