0eaca8293bc6f25be774b3517c555ea71331f1a8
DJ1 <jimmynoble604@gmail.com>
2023-11-16 19:05:22 -0500

    resource model.js completed

--- api/resource/model.js
+++ api/resource/model.js
@@ -2,7 +2,7 @@ const db = require('../../data/dbConfig')
 
 async function get() {
     const rows = await db('resources');
-    rerturn(rows)
+    return(rows)
 }
 
 async function create(resource) {
